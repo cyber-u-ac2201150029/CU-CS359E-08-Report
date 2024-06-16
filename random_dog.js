086e3c4f122d12e8a9f122c7487df55bdb2053e8
Juro Hinoto <2201150029xo@cyber-u.ac.jp>
2024-06-16 18:51:26 +0900

    第8回レポート提出用nodejsプログラム
    JavaScriptフレームワークによるWebプログラミングの第8回レポート課題提出用nodejsプログラム
    DogAPI（https://dog.ceo/dog-api/）から画像ファイルURLを抽出しコンソールに出力

---
+++ random_dog.js
@@ -0,0 +1,7 @@
+const request = require('request');
+request('https://dog.ceo/api/breeds/image/random', function (error, response, body) {
+    if (!error && response.statusCode == 200) {
+        const data = JSON.parse(body); console.log(body);
+        const dogImageUrl = data.message; console.log(dogImageUrl);
+    }
+});
